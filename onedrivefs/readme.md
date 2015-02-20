e9181b78da2a8fd26a92973b0e4b14ea757549fc
MonkeyIncinerator <kevin.f.rooney.jr@gmail.com>
2015-02-19 22:31:51 -0500

    Created the onedrive extension from the packager, and added the wl.js from live.com to bower.json

---
+++ onedrivefs/readme.md
@@ -0,0 +1,22 @@
+# onedrive
+
+## Overview
+
+Describe your provider here
+
+## Building
+
+First clone and configure this repository as described in the main readme. Then run the following commands.
+
+```bash
+$ cd onedrive
+$ npm install
+$ grunt
+```
+
+This will install all the dependencies and build the project. You can then compress the `extension` directory to a ZIP archive, or install the extension for testing as an unpacked extension by selecting the `extension` directory from Chrome's 'Load unpacked extension' dialog.
+
+## Testing
+
+To run the unit test suite, run `grunt test` from the top-level directory. Make sure you've followed all the setup instructions in the top-level readme first.
+
---
+++ onedrivefs/test/spec/events.js
@@ -0,0 +1,58 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+'use strict';
+
+var fs = 'onedrive';
+
+var events = require('../../js/events');
+
+var onCopyEntryRequested = events.onCopyEntryRequested;
+var onCreateFileRequested = events.onCreateFileRequested;
+var onCloseFileRequested = events.onCloseFileRequested;
+var onDeleteEntryRequested = events.onDeleteEntryRequested;
+var onGetMetadataRequested = events.onGetMetadataRequested;
+var onMoveEntryRequested = events.onMoveEntryRequested;
+var onOpenFileRequested = events.onOpenFileRequested;
+var onReadFileRequested = events.onReadFileRequested;
+var onReadDirectoryRequested = events.onReadDirectoryRequested;
+var onTruncateRequested = events.onTruncateRequested;
+var onWriteFileRequested = events.onWriteFileRequested;
+
+var testSuite;
+
+testSuite = require('../../../shared_tests/onCopyEntryRequested');
+testSuite(onCopyEntryRequested, onGetMetadataRequested);
+
+testSuite = require('../../../shared_tests/onCreateFileRequested');
+testSuite(onCreateFileRequested, onGetMetadataRequested);
+
+testSuite = require('../../../shared_tests/onCloseFileRequested');
+testSuite(fs, onCloseFileRequested, onOpenFileRequested);
+
+testSuite = require('../../../shared_tests/onDeleteEntryRequested');
+testSuite(fs, onDeleteEntryRequested, onGetMetadataRequested);
+
+testSuite = require('../../../shared_tests/onGetMetadataRequested');
+testSuite(onGetMetadataRequested);
+
+testSuite = require('../../../shared_tests/onMoveEntryRequested');
+testSuite(onMoveEntryRequested, onGetMetadataRequested);
+
+testSuite = require('../../../shared_tests/onOpenFileRequested');
+testSuite(fs, onOpenFileRequested);
+
+testSuite = require('../../../shared_tests/onReadFileRequested');
+testSuite(onReadFileRequested, onOpenFileRequested);
+
+testSuite = require('../../../shared_tests/onReadDirectoryRequested');
+testSuite(onReadDirectoryRequested);
+
+testSuite = require('../../../shared_tests/onTruncateRequested');
+testSuite(onTruncateRequested, onReadFileRequested, onOpenFileRequested);
+
+testSuite = require('../../../shared_tests/onWriteFileRequested');
+testSuite(onWriteFileRequested, onReadFileRequested, onOpenFileRequested);
